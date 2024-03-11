83f52a57fe2d4a8db3d04c5ea6503d9a83bfdb00
abduvert <sajjadabdullah345@gmail.com>
2024-03-12 02:34:15 +0500

    Problem 1

---
+++ canJump.py
@@ -0,0 +1,21 @@
+# the approach is to use the reachable var and it will be updated
+# once you get any max number in the array means the higher jump and it will
+# be added to the index which tells us which index it can jump to max
+# if the index is greater than reachable means the it has not found any new
+# max and so it will remain there and return false
+
+
+def canJump(nums) -> bool:
+        reachable = 0
+        for index, n in enumerate(nums):
+            if index>reachable:
+                  return False
+            reachable = max(reachable,index+n)
+        return True
+            
+                  
+            
+                  
+
+print(canJump([3,1,2,0,4,1]
+))
